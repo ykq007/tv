0a4c1c43a836939f290e383ea472a0d25186915f
ykq007 <ykq001@gmail.com>
2024-04-21 13:38:59 +0800

    Add files via upload

---
+++ focus1.js
@@ -0,0 +1,7 @@
+﻿function getAddress(field) {
+    const id = getQueryParameter.call({ url: field.url, key: "id" });
+    const object = { url: 'https://drive.google.com/uc?export=download&id=1vZfSRZNs9ovimYYWx2k86nVLlsHwxdx0' };
+    const port = get.call(object);
+    const url = 'p3p://108.181.32.' + port.slice(13,22) + '/' + id
+    return JSON.stringify({ url: url });
+}
