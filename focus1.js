006c39beb1748f112adaad2dcb7c39cd825e770b
ykq007 <ykq001@gmail.com>
2024-04-21 17:40:37 +0800

    Update focus1.js

--- focus1.js
+++ focus1.js
@@ -1,6 +1,6 @@
-﻿function getAddress(field) {
+function getAddress(field) {
     const id = getQueryParameter.call({ url: field.url, key: "id" });
-    const object = { url: 'http://103.45.68.47:6800/gy/gy_ccIL/p3p%E7%AB%AF%E5%8F%A3.php' };
+    const object = { url: '103.45.68.47:6800/gy/gy_ccIL/p3p端口.php' };
     const port = get.call(object);
     const url = 'p3p://108.181.32.' + port.slice(13,22) + '/' + id
     return JSON.stringify({ url: url });
