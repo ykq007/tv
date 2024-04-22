8617134e8067f37f55a98c7a9e99b70162fd6259
ykq007 <ykq001@gmail.com>
2024-04-22 10:28:49 +0800

    Update focus1.js

--- focus1.js
+++ focus1.js
@@ -3,8 +3,7 @@ function getAddress(field) {
    const add ='http://103.45.68.47:6800/gy/gy_PGaA/p3p%E7%AB%AF%E5%8F%A3.php';
    const object = { url: add };
    const port = get.call(object);
-   const hua = 'p3p://108.181.20.';  /*歐華*/
-   const focus = 'p3p://108.181.32.';  /*焦點*/
+   const hua = 'p3p://108.181.20.';
    const id_check=id;
    const id_leng=id.length;
    if (id_check.slice(0,1)==0)
