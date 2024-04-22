91a8b9bbc906a1303b2c24df54e047be737b2344
ykq007 <ykq001@gmail.com>
2024-04-23 00:09:34 +0800

    Update tv.js

--- tv.js
+++ tv.js
@@ -1,18 +1,7 @@
 function getAddress(field) {
-   const id = getQueryParameter.call({ url: field.url, key: "id" });
-   const add ='http://103.45.68.47:6800/gy/gy_PGaA/p3p%E7%AB%AF%E5%8F%A3.php';
-   const object = { url: add };
-   const port = get.call(object);
-   const hua = 'p3p://108.181.20.';
-   const id_check=id;
-   const id_leng=id.length;
-   if (id_check.slice(0,1)==0)
- {
- url = hua + port.slice(0,9) + '/' + id_check.slice(1,id_leng); 
- }
-else 
- {
-  url = focus + port.slice(13,22) + '/' + id;
-  }
-   return JSON.stringify({ url: url });
+    const id = getQueryParameter.call({ url: field.url, key: "id" });
+    const object = { url: 'http://103.45.68.47:6800/gy/gy_PGaA/p3p%E7%AB%AF%E5%8F%A3.php' };
+    const port = get.call(object);
+    const url = 'p3p://108.181.32.' + port.slice(13,22) + '/' + id
+    return JSON.stringify({ url: url });
 }
